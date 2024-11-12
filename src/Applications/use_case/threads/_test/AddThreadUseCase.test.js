3f87dde78cdb6dd215f378c1dc2447f6adc4f68c
Bayu Wicaksono <bwbayuuu@gmail.com>
2024-11-12 23:44:05 +0700

    feature/use case file for add thread

---
+++ src/Applications/use_case/threads/_test/AddThreadUseCase.test.js
@@ -0,0 +1,39 @@
+const Thread = require("../../../../Domains/threads/entities/Thread");
+const ThreadRepository = require("../../../../Domains/threads/ThreadRepository");
+const AddThreadUseCase = require("../AddThreadUseCase");
+const AddedThread1 = require('../../../../Domains/threads/entities/AddedThread');
+
+describe('AddThreadUseCase', () => {
+    it('should orchestrating the add thread action correctly', async () => {
+        const useCasePayload = {
+            title: 'thread 1',
+            body: 'isi thread 1',
+        };
+
+        const userPayload = 'user-123';
+
+        const mockAddedThread = new AddedThread1({
+            id: 'thread-123',
+            title: useCasePayload.title,
+            userId: userPayload,
+        });
+
+        const mockThreadRepository = new ThreadRepository();
+        mockThreadRepository.addThread = jest.fn()
+        .mockImplementation(() => Promise.resolve(mockAddedThread));
+
+        // use case instance
+        const getThreadUseCase = new AddThreadUseCase({
+            ThreadRepository: mockThreadRepository,
+        });
+
+        const AddedThread = await getThreadUseCase.execute(useCasePayload, userPayload);
+
+        // 
+        expect(AddedThread).toStrictEqual(new AddedThread1({
+            id: 'thread-123',
+            title: useCasePayload.title,
+            userId: userPayload,
+        }));
+    })
+});
