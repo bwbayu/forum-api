4fb44b48566e858f5e2dbb447f4f1dd3fed81cd3
Bayu Wicaksono <bwbayuuu@gmail.com>
2024-11-18 13:07:40 +0700

    feat/add entity files and test for reply

---
+++ src/Domains/replies/entities/_test/AddedReply.test.js
@@ -0,0 +1,33 @@
+const AddedReply = require("../AddedReply");
+
+describe('an AddedReply entity', () => {
+    it('should throw error when payload did not contain needed property', () => {
+        const payload = {};
+
+        expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when payload did not meet data type specification', () => {
+        const payload = {
+            id: 123,
+            content: 'This is a reply comment',
+            user_id: 456
+        };
+
+        expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+
+    it('should create addedReply object correctly', () => {
+        const payload = {
+            id: 'reply-123',
+            content: 'This is a reply comment',
+            user_id: 'user-123'
+        };
+
+        const addedReply = new AddedReply(payload);
+
+        expect(addedReply.id).toEqual(payload.id);
+        expect(addedReply.content).toEqual(payload.content);
+        expect(addedReply.user_id).toEqual(payload.user_id);
+    });
+});
---
+++ src/Domains/replies/entities/_test/NewReply.test.js
@@ -0,0 +1,33 @@
+const NewReply = require("../NewReply");
+
+describe('a reply entity', () => {
+    it('should throw error when payload did not contain needed property', () => {
+        const payload = {};
+
+        expect(() => new NewReply(payload)).toThrowError('NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when payload did not meet data type specification', () => {
+        const payload = {
+            content: 123,
+            user_id: 'user-123',
+            thread_id: 'thread-123',
+            comment_id: 'comment-123',
+        };
+
+        expect(() => new NewReply(payload)).toThrowError('NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+
+    it('should create reply object correctly', () => {
+        const payload = {
+            content: 'ini balasan komentar',
+            user_id: 'user-123',
+            thread_id: 'thread-123',
+            comment_id: 'comment-123',
+        };
+
+        const reply = new NewReply(payload);
+
+        expect(reply.content).toEqual(payload.content);
+    });
+})
